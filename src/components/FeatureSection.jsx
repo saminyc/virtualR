dc669c8c77cd6265283827b2f5ab7f4ac30a72ec
Samin Chowdhury <SAMIN.CHOWDHURY68@qmail.cuny.edu>
2024-09-11 12:42:47 -0400

    Started Working on Feature section

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FeatureSection = () => {
+		return (
+				<div className='relative mt-20 border-b border-neutral-600 min-h-[800px]'>
+					<div className="text-center">
+					
+							<span className='py-1 px-2 rounded-full h-6 text-sm font-medium uppercase bg-neutral-900 text-orange-600'>
+								Features
+								</span>
+					
+						
+					</div>
+				</div>
+		)
+}
+
+export default FeatureSection
